7fb638d401c401f59bdab9476cd62681681bb336
wanderer <mjbecze@gmail.com>
2016-09-11 13:02:42 -0400

    rebuild

--- client.js
+++ client.js
@@ -3,35 +3,49 @@ const html = require('choo/html')
 const sf = require('sheetify')
 const evm2wasm = require('evm2wasm')
 const ethUtil = require('ethereumjs-util')
-const sparser = require('s-expression')
 const app = choo()
 
-// add css
+// add global css
 sf('./client.css', { global: true })
 
+const scroll = sf`
+  :host {
+    overflow: scroll;
+    width: 70%;
+  }`
+
 const demoEVMcode = '0x60606040526000357c010000000000000000000000000000000000000000000000000000000090048063771602F7146037576035565b005b60546004808035906020019091908035906020019091905050606A565b6040518082815260200191505060405180910390f35b6000818301905080505b9291505056'
-const demoWastCode = compileEVM(demoEVMcode)
+const demoWastCode = compileEVM(demoEVMcode, true)
 
 app.model({
   state: {
-    wastCode: demoWastCode
+    evmCode: demoEVMcode,
+    wastCode: demoWastCode,
+    inlineOps: true
   },
   reducers: {
     compile: (data, state) => ({
-      wastCode: compileEVM(data)
+      evmCode: data,
+      wastCode: compileEVM(data, state.inlineOps)
+    }),
+    toggle: (data, state) => ({
+      inlineOps: !state.inlineOps
     })
   }
 })
 
-const mainView = (state, prev, send) => html`
+const mainView = (state, prev, send) => html `
   <main>
-    <h1>EVM 2 EWASM</h1>  transcompiles EVM bytecode to ewasm <br> <br>
-    <textarea 
-      rows="50"
-      cols="50"
-      onkeypress=${(e) => send('compile', e.target.value)}
-    >${demoEVMcode}</textarea>
+    <h1>EVM 2 EWASM</h1>
     <div>
+      <textarea rows="50"cols="50"onkeypress=${(e) => send('compile', e.target.value)}>${demoEVMcode}</textarea>
+      <br>
+      <input type="checkbox" checked=${state.inlineOps} onchange=${(e) => {
+        send('toggle')
+        send('compile', state.evmCode)
+      }}></input>inline EVM opcodes
+    </div>
+    <div class=${scroll}>
       <code>${state.wastCode}</code>
     </div>
   </main>`
@@ -43,24 +57,9 @@ app.router((route) => [
 const tree = app.start()
 document.body.appendChild(tree)
 
-function compileEVM (evm) {
-  return format(
-    sparser(
-      evm2wasm.compileEVM(
-        ethUtil.toBuffer(
-          evm), false, false).replace(/[^\x20-\x7E]/gmi, '')))
-}
-
-function format (sexp, depth = 0) {
-  depth++
-  let string = '('
-  sexp.forEach((i) => {
-    if (Array.isArray(i)) {
-      string += '\n' + ' '.repeat(depth * 2) + format(i, depth)
-    } else {
-      string += i + ' '
-    }
+function compileEVM (evm, inlineOps) {
+  return evm2wasm.compileEVM(ethUtil.toBuffer(evm), {
+    inlineOps: inlineOps,
+    pprint: true
   })
-  string = string.slice(0, -1)
-  return string + ')'
 }
